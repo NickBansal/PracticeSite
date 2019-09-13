9542449d5d6ac233cb46188b32db59f40b22f4c4
Nick Bansal <extreme_wired@msn.com>
2019-09-13 20:33:11 +0100

    added in the get request for the news feed

--- src/pages/Dashboard/NewsFeedCard/index.js
+++ src/pages/Dashboard/NewsFeedCard/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import Card from '../../../components/Card';
 import Button from '../../../components/Button';
 import { fontSize } from '../../../assets/globalStyles/constants/index';
 import { HR } from '../../../assets/globalStyles/index';
+import url from '../../../newsApiConfig';
 
 const Title = styled.h1`
     font-size: ${fontSize.title};
     margin: 0;
 `;
 
-const NewsFeedCard = () => (
-  <Card fadeIn="1.5s">
-      <Title>News feed</Title>
-      <HR />
-      <Button onClick={() => console.log('hello')}>Find out more</Button>
-    </Card>
-);
+const NewsFeedCard = () => {
+  const [newsFeed, setNewsFeed] = useState([]);
+
+  useEffect(() => {
+    axios.get(url)
+      .then((res) => {
+        setNewsFeed(res.data.articles);
+      });
+    // eslint-disable-next-line
+    }, newsFeed.slice(0, 10));
+  return (
+      <Card fadeIn="1.5s">
+          <Title>News feed</Title>
+          <HR />
+          <Button onClick={() => console.log('hello')}>Find out more</Button>
+        </Card>
+  );
+};
 
 export default NewsFeedCard;
