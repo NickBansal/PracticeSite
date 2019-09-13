80394bbb9b164b7ef4ad4f59bd873d1e74da7d3c
Nick Bansal <extreme_wired@msn.com>
2019-09-13 12:31:47 +0100

    lazy loader is working

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import backgroundImage from '../../assets/dashboard/thailand.JPG';
+
+const Background = styled(LazyLoadImage)`
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+  filter: grayscale(0.7);
+`;
 
 function App() {
   return (
-    <div />
+    <Background
+      alt="Ao Nang Beach"
+      src={backgroundImage} // use normal <img> attributes as props
+      effect="blur"
+      delayMethod="debounce"
+      delayTime={1000}
+    />
   );
 }
 
