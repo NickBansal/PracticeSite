a6ffc42bfd38446ae6603170c607bddd9fabd3d3
Nick Bansal <extreme_wired@msn.com>
2019-09-13 13:13:20 +0100

    added my own lazy image loader

---
+++ src/components/BlurImageLoader/index.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { string } from 'prop-types';
+
+const LoadableImage = styled.div`
+    background-image: url(${({ src }) => src});
+    filter: ${({ loaded }) => (!loaded ? 'blur(30px)' : 'grayscale(0.8)')};
+    width: ${({ width }) => width};
+    height: ${({ height }) => height};
+    transition: filter 2s ease;
+    background-position: 50% 50%;
+    background-origin: border-box;
+    background-size: cover;
+    flex-shrink: 0;
+`;
+
+const BlurImageLoader = ({ placeholder, image, ...props }) => {
+  const runOnce = true;
+
+  const [loadState, setLoadState] = useState({
+    src: placeholder,
+    loaded: false,
+  });
+
+  useEffect(() => {
+    const img = new Image();
+
+    img.onload = () => {
+      setLoadState({
+        src: img.src,
+        loaded: true,
+      });
+    };
+
+    img.src = image;
+  }, [runOnce]);
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <LoadableImage {...props} {...loadState} />;
+};
+
+BlurImageLoader.propTypes = {
+  placeholder: string.isRequired,
+  image: string.isRequired,
+};
+
+export default BlurImageLoader;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import BlurImageLoader from '../../components/BlurImageLoader';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import backgroundImage from '../../assets/dashboard/thailand.JPG';
 
-const Background = styled(LazyLoadImage)`
-  width: 100%;
-  height: 100vh;
-  object-fit: cover;
-  filter: grayscale(0.7);
-`;
-
-function App() {
-  return (
-    <Background
-      alt="Ao Nang Beach"
-      src={backgroundImage} // use normal <img> attributes as props
-      effect="blur"
-      delayMethod="debounce"
-      delayTime={1000}
-    />
-  );
-}
+const App = () => (
+  <BlurImageLoader
+    width="100%"
+    height="100vh"
+    image={backgroundImage}
+    alt="Thailand picture"
+    placeholder="Thailand picture"
+  />
+);
 
 export default App;
