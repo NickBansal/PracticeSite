97e1bb8bd0c573c5f0aba435877f46c1fbf9cc9f
Nick Bansal <extreme_wired@msn.com>
2019-09-21 20:49:14 +0100

    notifys the user which country news they are viewing

--- src/pages/Dashboard/NewsFeedCard/index.js
+++ src/pages/Dashboard/NewsFeedCard/index.js
@@ -8,7 +8,6 @@ import { fontSize } from '../../../assets/globalStyles/constants/index';
 import { HR, LiveText } from '../../../assets/globalStyles/index';
 import NewsSelect from './NewsSelect';
 import flags from '../../../assets/flags';
-import BlurImageLoader from '../../../components/BlurImageLoader';
 
 const endpoint = 'http://127.0.0.1:8080/';
 
@@ -28,10 +27,15 @@ const TitleContainer = styled.div`
     justify-content: space-between;
 `;
 
+const Image = styled.img`
+    height: 30px;
+    width: 30px;
+    border-radius: 4px;
+`;
+
 const NewsFeedCard = () => {
     const [newsFeed, setNewsFeed] = useState([]);
     const [country, setCountry] = useState('united-kingdom');
-
     const [modal, setModal] = useState(false);
 
     useEffect(() => {
@@ -47,20 +51,16 @@ const NewsFeedCard = () => {
         setCountry(value);
     };
 
-    console.log(country);
-
     const currentCountry = flags.filter((flag) => JSON.stringify(flag).includes(country));
 
     return (
         <Card fadeIn="1.5s">
             <TitleContainer>
                 <Title><LiveText>Live</LiveText> news feed</Title>
-                <BlurImageLoader
-                    width="30px"
-                    height="30px"
-                    image={currentCountry[0]}
-                    alt={`${currentCountry} flag`}
-                    placeholder={`${currentCountry} flag`}
+                <Image
+                    src={currentCountry[0]}
+                    alt={`${country} flag`}
+                    placeholder={`${country} flag`}
                 />
             </TitleContainer>
             <HR />
