47ee77b579902b4944d2f5a7995b3f4d1c3d02f2
Nick Bansal <extreme_wired@msn.com>
2019-09-22 12:35:31 +0100

    added new flags

--- src/assets/flags/index.js
+++ src/assets/flags/index.js
@@ -4,14 +4,20 @@ import austria from './austria.svg';
 import brazil from './brazil.svg';
 import canada from './canada.svg';
 import china from './china.svg';
+import france from './france.svg';
 import germany from './germany.svg';
 import greece from './greece.svg';
 import india from './india.svg';
+import ireland from './ireland.svg';
 import israel from './israel.svg';
+import italy from './italy.svg';
+import newZealand from './new-zealand.svg';
 import norway from './norway.svg';
+import portugal from './portugal.svg';
 import southKorea from './south-korea.svg';
 import switzerland from './switzerland.svg';
 import thailand from './thailand.svg';
+import uae from './united-arab-emirates.svg';
 import uk from './united-kingdom.svg';
 import us from './united-states-of-america.svg';
 
@@ -22,14 +28,20 @@ export default [
     brazil,
     canada,
     china,
+    france,
     germany,
     greece,
     india,
+    ireland,
     israel,
+    italy,
+    newZealand,
     norway,
+    portugal,
     southKorea,
     switzerland,
     thailand,
+    uae,
     uk,
     us,
 ];
