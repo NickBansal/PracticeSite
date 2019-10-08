0658ba01ce44e036e558da7d403e739ce0eae9f6
Nick Bansal <extreme_wired@msn.com>
2019-10-08 15:31:47 +0100

    updated validation for yup

--- src/pages/Dashboard/LiveChatCard/ChatForm/schema.js
+++ src/pages/Dashboard/LiveChatCard/ChatForm/schema.js
@@ -1,18 +1,24 @@
 import * as yup from 'yup';
-import alphaNumeric from '../../../../utils/validation/alphaNumeric';
+import get from 'lodash.get';
+
+const regex = /^[a-zA-Z0-9()',+-\s]+$/;
+
+const alphaNumeric = options => {
+	const max = get(options, 'max', 50);
+	const message = get(options, 'message', 'Alpha numeric characters only');
+
+	return yup
+		.string()
+		.trim()
+		.max(max)
+		.matches(regex, { message })
+		.required();
+};
 
 const schema = yup
 	.object({
-		username: alphaNumeric({
-			max: 50
-		})
-			.required()
-			.label('Username'),
-		room: alphaNumeric({
-			max: 50
-		})
-			.required()
-			.label('Chat room')
+		username: alphaNumeric().label('Username'),
+		room: alphaNumeric().label('Chat room')
 	})
 	.required();
 
