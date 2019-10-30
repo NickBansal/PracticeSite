c8e7a12ee89f54dd0c7a705ab196dc705d3a8cbf
Nick Bansal <extreme_wired@msn.com>
2019-10-30 09:56:05 +0000

    added animation effects on each sound

--- src/pages/Dashboard/DrumMachineCard/DrumLoop/index.js
+++ src/pages/Dashboard/DrumMachineCard/DrumLoop/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { colors, spacing } from '../../../../utils/globalStyles/constants';
 
-const WhiteBox = styled.div`
+const DrumContainer = styled.div`
 	height: 500px;
-	background: white;
 	min-width: 700px;
+	display: flex;
+	align-items: center;
 `;
 
 const Controls = styled.div`
@@ -15,11 +16,58 @@ const Controls = styled.div`
 	height: 40px;
 `;
 
-const DrumLoop = () => (
-	<>
-		<WhiteBox />
-		<Controls />
-	</>
-);
+const Labels = styled.div`
+	height: 100%;
+	width: 15%
+	background: ${colors.orange};
+`;
+
+const Drums = styled.div`
+	width: 550px;
+	width: 85%;
+`;
+
+const Column = styled.div`
+	display: inline-block;
+`;
+
+const Sound = styled.div`
+	min-width: 35px;
+	height: 45px;
+	border: 1px solid #ffffff66;
+
+	&:hover {
+		cursor: pointer;
+		border: 1px solid white;
+		background: black;
+	}
+
+	&:active {
+		transform: scale(1.1);
+	}
+
+	transition: transform 0.3s;
+`;
+
+const DrumLoop = () => {
+	const [drums, setDrums] = useState(Array(16).fill(Array(8).fill(null)));
+	return (
+		<>
+			<DrumContainer>
+				<Labels />
+				<Drums>
+					{drums.map((column, index) => (
+						<Column key={String(index)}>
+							{column.map((sound, i) => (
+								<Sound key={String(i)}>{sound}</Sound>
+							))}
+						</Column>
+					))}
+				</Drums>
+			</DrumContainer>
+			<Controls />
+		</>
+	);
+};
 
 export default DrumLoop;
