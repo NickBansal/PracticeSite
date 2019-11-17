ea92697855f25520e7e8773afe1bf0a2cea11222
Nick Bansal <extreme_wired@msn.com>
2019-11-17 09:43:23 +0000

    added food

--- src/pages/Dashboard/SnakeCard/SnakeGame/index.js
+++ src/pages/Dashboard/SnakeCard/SnakeGame/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { colors, breakPoints } from '../../../../utils/globalStyles/constants';
+import { createEmptyGameBoard, generateRandomFood } from './utils';
 import useInterval from '../../../../utils/hooks/useInterval';
 // import pint from '../../../../assets/snake/pint.svg';
 
@@ -32,23 +33,17 @@ const Container = styled.div`
 
 const rowsLength = 15;
 
-const createEmptyGameBoard = rows => {
-	const arr = [];
-	for (let i = 0; i < rows; i++) {
-		arr[i] = Array(rows).fill(0);
-	}
-	return arr;
-};
-
 const SnakeGame = () => {
 	const [grid, setGrid] = useState(createEmptyGameBoard(rowsLength));
 	const [snake, setSnake] = useState([[7, 7], [8, 7]]);
+	const [food, setFood] = useState(generateRandomFood(grid, rowsLength));
 
 	const updateBoard = () => {
 		const newGrid = grid.slice();
 		snake.forEach(([x, y]) => {
 			newGrid[x][y] = 1;
 		});
+		newGrid[food[0]][food[1]] = 2;
 		setGrid(newGrid);
 	};
 
@@ -60,15 +55,14 @@ const SnakeGame = () => {
 
 	// useInterval(moveSnake, 500);
 
-	const [x, y] = snake[0];
-
 	return (
 		<Container id="snakeGame">
 			{grid.map((col, i) => (
 				<Column key={String(i)}>
-					{col.map((row, j) => (
-						<Cells key={String(j)} snake={row === 1} />
-					))}
+					{col.map((row, j) => {
+						console.log(row);
+						return <Cells key={String(j)} snake={row === 1} />;
+					})}
 				</Column>
 			))}
 		</Container>
---
+++ src/pages/Dashboard/SnakeCard/SnakeGame/utils.js
@@ -0,0 +1,13 @@
+export const createEmptyGameBoard = rows => {
+	const arr = [];
+	for (let i = 0; i < rows; i++) {
+		arr[i] = Array(rows).fill(0);
+	}
+	return arr;
+};
+
+export const generateRandomFood = (grid, rows) => {
+	const i = Math.floor(Math.random() * rows);
+	const j = Math.floor(Math.random() * rows);
+	return grid[i][j] === 0 ? [i, j] : generateRandomFood(grid);
+};
