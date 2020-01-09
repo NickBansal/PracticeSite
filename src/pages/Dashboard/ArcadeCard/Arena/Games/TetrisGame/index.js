bf6f8e194c90256770ba5c40c22c8f33dbdfbf42
Nick Bansal <extreme_wired@msn.com>
2020-01-09 16:45:54 +0000

    added in random pieces selection

--- src/pages/Dashboard/ArcadeCard/Arena/Games/TetrisGame/index.js
+++ src/pages/Dashboard/ArcadeCard/Arena/Games/TetrisGame/index.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { colors } from '../../../../../../utils/globalStyles/constants';
 import CONSTANTS from '../../../../../../constants';
+import pieces from './pieces';
 
 import Information from './Information';
 
@@ -18,27 +19,48 @@ const Canvas = styled.canvas`
 	border: 5px solid ${colors.pink};
 `;
 
-let myReq;
+// function renderGame(context) {
+// 	matrix.forEach((row, y) => {
+// 		context.scale(pixelRatio, pixelRatio);
+// 		row.forEach((value, x) => {
+// 			if (value !== 0) {
+// 				context.fillStyle = 'red';
+// 				context.fillRect(x, y, 1, 1);
+// 			}
+// 		});
+// 	});
+// function update(time = 0) {
+// 	console.log(time);
+// 	myReq = window.requestAnimationFrame(update);
+// }
+// update();
+// }
 
-function createEngine() {
-	function update(time = 0) {
-		console.log(time);
-		myReq = window.requestAnimationFrame(update);
-	}
-	update();
-}
+const matrix = () => pieces[Math.floor(Math.random() * pieces.length + 1)][0];
+
+const drawShape = context => {
+	context.scale(10, 10);
+	matrix().forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value !== 0) {
+				context.fillStyle = 'red';
+				context.fillRect(x, y, 1, 1);
+			}
+		});
+	});
+};
 
 const TetrisGame = () => {
-	const canvas = useRef(null);
+	const ref = useRef();
 
 	useEffect(() => {
-		const context = canvas.current.getContext('2d');
+		const context = ref.current.getContext('2d');
 		context.scale(PIXEL_RATIO, PIXEL_RATIO);
 		context.fillStyle = colors.black;
-		context.fillRect(0, 0, TETRIS_WIDTH, TETRIS_HEIGHT);
-
-		createEngine();
-		return () => window.cancelAnimationFrame(myReq);
+		context.fillRect(0, 0, ref.current.width, ref.current.height);
+		context.stroke();
+		drawShape(context);
+		// return () => window.cancelAnimationFrame(myReq);
 		// eslint-disable-next-line
 	}, []);
 
@@ -47,7 +69,7 @@ const TetrisGame = () => {
 	return (
 		<Container>
 			<Canvas
-				ref={canvas}
+				ref={ref}
 				width={TETRIS_WIDTH}
 				height={TETRIS_HEIGHT}
 				style={style}
