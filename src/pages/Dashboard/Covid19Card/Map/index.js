53fd3aa92283e08879ba5b3822ddf7675e18add3
Nick Bansal <extreme_wired@msn.com>
2020-06-11 15:45:19 +0100

    added in the google maps

--- src/pages/Dashboard/Covid19Card/Map/index.js
+++ src/pages/Dashboard/Covid19Card/Map/index.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import axios from 'axios';
 
+import API_KEY from '../../../../keys/googleAPI';
+
 const Map = () => {
 	const [latest, SetLatest] = useState([]);
 	const [results, SetResults] = useState([]);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		axios
@@ -16,11 +19,27 @@ const Map = () => {
 				SetLatest(res[0].data);
 				SetResults(res[1].data);
 			})
-			.catch(console.log);
+			.catch(err => setError(err));
 	}, []);
-	console.log(latest);
-	console.log(results);
-	return null;
+
+	return (
+		<div style={{ height: '100vh', width: '100%' }}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: API_KEY }}
+				defaultCenter={{
+					lat: 59.95,
+					lng: 30.33
+				}}
+				defaultZoom={10}
+			>
+				{/* <AnyReactComponent
+					lat={59.955413}
+					lng={30.337844}
+					text="My Marker"
+				/> */}
+			</GoogleMapReact>
+		</div>
+	);
 };
 
 export default Map;
