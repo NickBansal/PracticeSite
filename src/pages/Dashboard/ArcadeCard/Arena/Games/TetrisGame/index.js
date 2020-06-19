9c39a17ce7e2204689b38e2cec0394bd606a36c4
Nick Bansal <extreme_wired@msn.com>
2020-06-19 14:10:02 +0100

    renewed tetris game

--- src/pages/Dashboard/ArcadeCard/Arena/Games/TetrisGame/index.js
+++ src/pages/Dashboard/ArcadeCard/Arena/Games/TetrisGame/index.js
@@ -1,11 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../../../../../../utils/globalStyles/constants';
-import CONSTANTS from '../../../../../../constants';
 
 import Information from './Information';
-
-const { TETRIS_HEIGHT, TETRIS_WIDTH, PIXEL_RATIO } = CONSTANTS;
 
 const Container = styled.div`
 	display: flex;
@@ -14,44 +11,17 @@ const Container = styled.div`
 	justify-content: center;
 `;
 
-const Canvas = styled.canvas`
+const Canvas = styled.div`
 	border: 5px solid ${colors.pink};
+	height: 580px;
+	width: 290px;
+	background: black;
 `;
 
-let myReq;
-
-function createEngine() {
-	function update(time = 0) {
-		myReq = window.requestAnimationFrame(update);
-	}
-	update();
-}
-
 const TetrisGame = () => {
-	const canvas = useRef(null);
-
-	useEffect(() => {
-		const context = canvas.current.getContext('2d');
-		context.scale(PIXEL_RATIO, PIXEL_RATIO);
-		context.fillStyle = colors.black;
-		context.fillRect(0, 0, TETRIS_WIDTH, TETRIS_HEIGHT);
-
-		createEngine();
-		return () => window.cancelAnimationFrame(myReq);
-		// eslint-disable-next-line
-	}, []);
-
-	const style = { TETRIS_WIDTH, TETRIS_HEIGHT };
-
 	return (
 		<Container>
-			<Canvas
-				ref={canvas}
-				width={TETRIS_WIDTH}
-				height={TETRIS_HEIGHT}
-				style={style}
-				data-testid="tetris"
-			/>
+			<Canvas />
 			<Information />
 		</Container>
 	);
